f3ee75255643ed18ec66c76038a62571e346dee6
whogaurab <gaurabchoudhary2002@gmail.com>
2024-06-13 08:25:59 +0530

    at 8:25  13-06-2024

---
+++ PlantDiseaseDetection/constant/training_pipeline/__init__.py
@@ -0,0 +1,14 @@
+ARTIFACTS_DIR: str = "artifacts"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1EFvLbg0y68SRrXNCeiUFB35B2Xku72QK/view?usp=drive_link"
+
+
+
+
